9e1ff2285eaf0578a0ad6f897f4a9816c7c15936
ds <benbryson789@users.noreply.github.com>
2021-10-10 10:18:54 -0400

    firebase with forum

--- src/pages/AddForum.js
+++ src/pages/AddForum.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React,{useCallback} from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import SidebarNav from '../components/Sidebar'
-
+// getFirestore is database
+// serverTimestamp is date
+// addDoc is row
+// collection is a table
+import {getFirestore,serverTimestamp,addDoc,collection} from "firebase/firestore";
+// useAuthState getting user ID from who is log in from the system
+import { useAuthState } from '../firebase';
+import { useHistory } from 'react-router';
 const AddForum = () => {
+    const db = getFirestore();
+    // getting login user data
+    const {user} = useAuthState();
+    const history = useHistory();
+    //console.log(serverTimestamp())
+    const handleSubmit = useCallback(async e => {
+                e.preventDefault();
+                // getting title and description when user submits the form
+                const{title,description} = e.target.elements;
+                try {
+                    // then get login user id
+                    let uid = user.uid;
+                    // we are inserting a record in firebase collection/table
+                    await addDoc(collection(db,"forum/default/discussion"),{
+                        uid:uid,
+                        title:title.value,
+                        description:description.value,
+                        timestamp:serverTimestamp()
+                    });
+                    alert("Succesfully added your form")
+                    history.push('/forum');
+                } catch (error) {
+                    alert(error.message);
+                }
+        },
+        [db,user,history])
     return (
         <>
             <Navbar/>
@@ -12,7 +45,7 @@ const AddForum = () => {
                         <SidebarNav/>
                         <div className="col-md-9">
                             <h2>Add Forum</h2>
-                            <form>
+                            <form method="post" onSubmit={handleSubmit}> 
                                 <div className="form-group">
                                     <label>Title</label>
                                     <input type="text" name="title" className="form-control" />
@@ -22,7 +55,7 @@ const AddForum = () => {
                                     <textarea className="form-control" rows="8" name="description"></textarea>
                                 </div>
                                 <div className="form-group">
-                                    <button className="btn btn-primary">Add Forum</button>
+                                    <button className="btn btn-primary" type="submit">Add Forum</button>
                                 </div>
                             </form>
                         </div>
--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -1,4 +1,10 @@
-import React from 'react'
+// collection is table
+// getDocs row
+// getFirestone is database
+// limit, orderBy are query commands
+// query is fetching data from Firebase
+import { collection, getDocs, getFirestore, limit, orderBy, query } from '@firebase/firestore'
+import React,{useState,useEffect} from 'react'
 import { Link,useHistory } from 'react-router-dom'
 import { Icon } from 'semantic-ui-react'
 import Footer from '../components/Footer'
@@ -7,12 +13,39 @@ import SidebarNav from '../components/Sidebar'
 
 const Forum = () => {
     const history = useHistory();
+    const[forumList,setForumList] = useState([]);
+    // console.log(forumList);
+    // edit function
     const handleClick = (id,type)=>{
         history.push("/forum/"+id+"/"+type);
     }
     const handleDelete = (id)=>{
         alert("Forum Deleted Succesfully");
     }
+    const db = getFirestore();
+    useEffect(() => {
+        const getForumList = async()=>{
+          // collection is firebase table and db is object of database of firebase
+            const totalForum = collection(db,"forum/default/discussion");
+            // run a query to fetch data from a specific database collecton
+            const q = query(totalForum,orderBy("timestamp","desc"),limit(10));
+            // after run fetch all  data from generated query
+            const itemList = await getDocs(q);
+            // declare blank array variable  and then
+            // getting single record and pushing it array variable and
+            // then setting up variable in state in line 42 or setForumList(results)
+            let results = [];
+            itemList.forEach((doc)=>{
+                results.push(doc.data());
+            })
+           // console.log(results);
+            setForumList(results);
+            // return results to array variable
+            return results;
+        }
+        getForumList()
+        
+    }, [setForumList,db])
     return (
         <>
             <Navbar/>
@@ -34,8 +67,9 @@ const Forum = () => {
             </tr>
           </thead>
           <tbody>
-           <tr>
-            <td>Washington DC<br/><b>Posted By : Debra Shelby</b><br/><Icon name="comments"/> This is great topic...</td>
+            { forumList && forumList.map((data,index)=>(
+          <tr key={index} className={ ((index+1)%2) === 0 ? '':'active'}>
+            <td>{data.title}<br/><b>Posted By : Debra Shelby</b><br/><Icon name="comments"/> {data.description.substring(0,30)}..</td>
             <td align="center">1</td>
             <td>July 7 - 9, 2021<br/><br/>
             <span className="controls">
@@ -45,61 +79,8 @@ const Forum = () => {
             </span>
             </td>
           </tr>
-          <tr className="active">    
-            <td>NSTP Annual Federal Tax Refresher Course<br/><b>Posted By : John</b><br/><Icon name="comments"/> This is very helpfull topic...</td>
-            <td align="center">12</td>
-            <td>July 10, 2015<br/><br/>
-            <span className="controls">
-                <Icon name="edit" onClick={()=>{handleClick(12,'edit')}}  className="edit"/>
-                <Icon name="trash" onClick={()=>{handleDelete(1)}} className="trash"/>
-                <Icon name="eye" className="view"/>
-            </span>
-            </td>
-          </tr>           
-          <tr>
-            <td>Washington DC<br/><b>Posted By : Debra Shelby</b><br/><Icon name="comments"/> This is great topic...</td>
-            <td align="center">1</td>
-            <td>July 7 - 9, 2021<br/><br/>
-            <span className="controls">
-                <Icon name="edit" onClick={()=>{handleClick(13,'edit')}}  className="edit"/>
-                <Icon name="trash" onClick={()=>{handleDelete(1)}} className="trash"/>
-                <Icon name="eye" className="view"/>
-            </span>
-            </td>
-          </tr>
-          <tr className="active">    
-            <td>NSTP Annual Federal Tax Refresher Course<br/><b>Posted By : John</b><br/><Icon name="comments"/> This is very helpfull topic...</td>
-            <td align="center">12</td>
-            <td>July 10, 2015<br/><br/>
-            <span className="controls">
-                <Icon name="edit"  onClick={()=>{handleClick(14,'edit')}} className="edit"/>
-                <Icon name="trash" onClick={()=>{handleDelete(1)}} className="trash"/>
-                <Icon name="eye" className="view"/>
-            </span>
-            </td>
-          </tr>  
-          <tr>
-            <td>Washington DC<br/><b>Posted By : Debra Shelby</b><br/><Icon name="comments"/> This is great topic...</td>
-            <td align="center">1</td>
-            <td>July 7 - 9, 2021<br/><br/>
-            <span className="controls">
-                <Icon name="edit" onClick={()=>{handleClick(15,'edit')}}  className="edit"/>
-                <Icon name="trash" onClick={()=>{handleDelete(1)}} className="trash"/>
-                <Icon name="eye" className="view"/>
-            </span>
-            </td>
-          </tr>
-          <tr className="active">    
-            <td>NSTP Annual Federal Tax Refresher Course<br/><b>Posted By : John</b><br/><Icon name="comments"/> This is very helpfull topic...</td>
-            <td align="center">12</td>
-            <td>July 10, 2015<br/><br/>
-            <span className="controls">
-                <Icon name="edit"   onClick={()=>{handleClick(16,'edit')}}className="edit"/>
-                <Icon name="trash" onClick={()=>{handleDelete(1)}} className="trash"/>
-                <Icon name="eye" className="view"/>
-            </span>
-            </td>
-          </tr>  
+            ))}
+          
         
         </tbody>
       </table>
